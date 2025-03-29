1f214a95a2f9b05c3e366211bf6e6f4dffc5f9ec
Dmitry Vegel <dm.vegel@gmail.com>
2025-03-29 14:26:49 +0300

    29032025 Добавление проверок, рефакторинг

--- src/com/javaops/webapp/storage/ArrayStorage.java
+++ src/com/javaops/webapp/storage/ArrayStorage.java
@@ -6,7 +6,11 @@ import com.javaops.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+
+    public static final String RESUME_PRESENT_MESSAGE = "Резюме с uuid = %s уже существует в хранилище";
+    public static final String RESUME_ABSENT_MESSAGE = "Резюме с uuid = %s отсутствует в хранилище";
+    public static final String STORAGE_EXCESS_MESSAGE = "Превышен размер хранилища";
 
     private int size;
 
@@ -18,43 +22,50 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        if (!checkExistence(r.getUuid())) {
-            System.out.println("Обновление невозможно: резюме отсутствует в хранилище");
+        if (getPosition(r.getUuid()) < 0) {
+            System.out.printf((RESUME_ABSENT_MESSAGE) + "%n", r.getUuid());
+            return;
         }
     }
 
-    public boolean checkExistence(String uuid) {
+    public int getPosition(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
+
     public void save(Resume r) {
         if (size == storage.length) {
-            System.out.println("Превышен размер хранилища");
-        } else {
-            storage[size++] = r;
+            System.out.println(STORAGE_EXCESS_MESSAGE);
+            return;
         }
+        if (getPosition(r.getUuid()) >= 0) {
+            System.out.printf((RESUME_PRESENT_MESSAGE) + "%n", r.getUuid());
+            return;
+        }
+        storage[size++] = r;
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
-                return storage[i];
-            }
+        int resumePosition = getPosition(uuid);
+        if (resumePosition >= 0) {
+            return storage[resumePosition];
         }
+        System.out.printf((RESUME_ABSENT_MESSAGE) + "%n", uuid);
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
-                storage[i] = storage[--size];
-                storage[size] = null;
-            }
+        int resumePosition = getPosition(uuid);
+        if (resumePosition >= 0) {
+            storage[resumePosition] = storage[--size];
+            storage[size] = null;
+            return;
         }
+        System.out.printf((RESUME_ABSENT_MESSAGE) + "%n", uuid);
     }
 
     /**
