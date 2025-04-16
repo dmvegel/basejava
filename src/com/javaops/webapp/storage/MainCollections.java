1b7ced8e86b0d08f0408e0b36c1de51379011109
Dmitry Vegel <dm.vegel@gmail.com>
2025-04-16 14:34:38 +0300

    16042025 Добавлен класс MainCollections

---
+++ src/com/javaops/webapp/storage/MainCollections.java
@@ -0,0 +1,12 @@
+package com.javaops.webapp.storage;
+
+import com.javaops.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MainCollections {
+    public static void main(String[] args) {
+        Collection<Resume> collection = new ArrayList<>();
+    }
+}
