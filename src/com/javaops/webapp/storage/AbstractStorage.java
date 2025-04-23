4848a1bb5ed74747c25b2440deea93ddb65d9516
Dmitry Vegel <dm.vegel@gmail.com>
2025-04-24 00:04:32 +0300

    24042025 Доработка AbstractStorage

--- src/com/javaops/webapp/storage/AbstractStorage.java
+++ src/com/javaops/webapp/storage/AbstractStorage.java
@@ -1,6 +1,39 @@
 package com.javaops.webapp.storage;
 
-public abstract class AbstractStorage implements Storage {
+import com.javaops.webapp.model.Resume;
+
+public abstract class AbstractStorage<T> implements Storage {
+    T searchKey;
+
+    @Override
+    public Resume get(String uuid) {
+        return doGet(searchKey);
+    }
+
+    @Override
+    public void save(Resume r) {
+        doSave(r, searchKey);
+        size++;
+    }
+
+    @Override
+    public void update(Resume r) {
+        doUpdate(r, searchKey);
+    }
+
+    @Override
+    public void delete(String uuid) {
+        doDelete(searchKey);
+        size--;
+    }
+
+    protected abstract Resume doGet(T searchKey);
+
+    protected abstract void doSave(Resume resume, T searchKey);
+
+    protected abstract void doUpdate(Resume resume, T searchKey);
+
+    protected abstract void doDelete(T searchKey);
 
     protected int size;
 
